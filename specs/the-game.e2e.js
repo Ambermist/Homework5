9725f1cb21fd4ad9273e3b47df1b4b95a6610060
Polina <palina.varonina@gmail.com>
2021-12-14 19:18:53 +0300

    game task

--- specs/the-game.e2e.js
+++ specs/the-game.e2e.js
@@ -1,10 +1,29 @@
+const compareX = async function(){
+    const ball = await $('#ball');
+    const pad = await $('#pad');
+    const ballX = (await ball.getLocation())['x'];
+    const padX = (await pad.getLocation())['x'];
+    let times;
+    if(padX < ballX){
+        times = Math.ceil((ballX - padX)/20);
+        for(let i = 0; i < times; i++){
+            await browser.keys('D');            
+        }
+    }
+    else {
+        times = Math.ceil((padX - ballX)/20);
+        for(let i = 0; i < times; i++){
+            await browser.keys('A');            
+        }
+    }
+}
 describe('The game', function () {
     it('should get more than 100 points ', async function () {
-        await browser.url('http://localhost:63342/course-sut/arkanoid.html');
+        await browser.url('https://viktor-silakov.github.io/course-sut/arkanoid.html');
         await browser.pause(1000);
         await $('button=PLAY').click();
         await browser.waitUntil(async () => {
-            // player logic
+            await compareX();
             const points = parseInt(await $('#points').getText(), 10);
             if (points > 100) return true
             console.log({ points })
